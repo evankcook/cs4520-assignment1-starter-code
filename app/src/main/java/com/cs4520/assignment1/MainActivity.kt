4c1c8a6a33065a527f5ce9f3b2edbdfc265a5a8b
Evan Cook <cook.evank@gmail.com>
2024-02-10 18:45:29 -0500

    add layout for login fragment

---
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.cs4520.assignment1
+
+class MainActivity {
+}
---
+++ app/src/main/java/com/cs4520/assignment1/ProductListFragment.kt
@@ -0,0 +1,8 @@
+package com.cs4520.assignment1;
+
+import androidx.fragment.app.Fragment;
+
+
+class ProductListFragment : Fragment() {
+
+}
