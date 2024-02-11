3b60a6bb8b2ccfd2ee4a605eef76fe204028d6b5
Evan Cook <cook.evank@gmail.com>
2024-02-11 16:36:40 -0500

    fix hidden appbar error

--- app/src/main/java/com/cs4520/assignment1/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -1,4 +1,12 @@
 package com.cs4520.assignment1
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
 }
