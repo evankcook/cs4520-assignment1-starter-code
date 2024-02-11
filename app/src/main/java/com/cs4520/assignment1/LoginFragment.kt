c21ae7d76780ee0f6ac82d60b2ca0216a4369efa
Evan Cook <cook.evank@gmail.com>
2024-02-11 16:39:55 -0500

    finish setting up entire nav

--- app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -1,6 +1,45 @@
 package com.cs4520.assignment1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment1.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding = LoginFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.loginButton)
+        button.apply{
+            setOnClickListener {
+                val username = view.findViewById<EditText>(R.id.usernameInput)
+                val password = view.findViewById<EditText>(R.id.passwordInput)
+                if (username.text.toString() == "admin" && password.text.toString() == "admin") {
+                    username.text.clear()
+                    password.text.clear()
+                findNavController().navigate(R.id.action_loginFragment_to_productListFragment)
+                } else {
+                    Toast.makeText(context, "Incorrect password. Please try again.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/cs4520/assignment1/ProductListFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/ProductListFragment.kt
@@ -1,8 +1,22 @@
 package com.cs4520.assignment1;
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment;
+import com.cs4520.assignment1.databinding.ProductListFragmentBinding
 
 
 class ProductListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding = ProductListFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
 }
